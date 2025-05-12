92a9bde9c4e7ff7b02cc815be5474c1fdc58106f
Roilan Lauzardo <125706938+rls1990@users.noreply.github.com>
2025-05-12 17:25:02 -0400

    TextSlider interval

--- src/components/home/header/top/HeaderTop.tsx
+++ src/components/home/header/top/HeaderTop.tsx
@@ -7,7 +7,7 @@ const HeaderTop = async () => {
   return (
     <div className="bg-emerald-700 text-white text-xs py-1.5 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto flex justify-between items-center flex-wrap gap-x-4 gap-y-1">
-        <TextSlider items={news} className="hidden sm:block" interval={5000} />
+        <TextSlider items={news} className="hidden sm:block" interval={7000} />
         <div>asdsad</div>
       </div>
     </div>
